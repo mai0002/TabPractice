9143d548d8555888f503223cfd9f8935deb58a58
MaiInagaki <maiinagaki@maiinagakinomacbook-puro.local>
2018-08-31 18:56:44 +0900

    エラーあり。調査開始前に一度コミット

---
+++ TabPractice/Sample2ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  Sample2ViewController.swift
+//  TabPractice
+//
+//  Created by MaiInagaki on 2018/08/31.
+//  Copyright © 2018年 MaiInagaki. All rights reserved.
+//
+
+import UIKit
+
+class Sample2ViewController: UIViewController {
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("Sample2ViewController Will Appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Sample2ViewController Will Disappear")
+    }
+    
+}
+
--- TabPractice/ViewController.swift
+++ TabPractice/ViewController.swift
@@ -9,17 +9,75 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Property
+    
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    private lazy var sample1ViewController: Sample1ViewController = {
+        let storyborad = UIStoryboard(name: "Main", bundle: Bundle.main)
+        var viewController = storyborad.instantiateViewController(withIdentifier: "Sample1ViewController") as! Sample1ViewController
+        add(asChildViewController: viewController)
+        return viewController
+    }()
+    
+    private lazy var sample2ViewController: Sample2ViewController = {
+        let storyborad = UIStoryboard(name: "Main", bundle: Bundle.main)
+        var viewController = storyborad.instantiateViewController(withIdentifier: "Sample2ViewController") as! Sample2ViewController
+        add(asChildViewController: viewController)
+        return viewController
+    }()
+    
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - View Methods
+    
+    private func setupView() {
+        updateView()
     }
-
-
+    
+    private func updateView() {
+        if segmentedControl.selectedSegmentIndex == 0 {
+            remove(asChildViewController: sample2ViewController)
+            add(asChildViewController: sample1ViewController)
+        } else {
+            remove(asChildViewController: sample1ViewController)
+            add(asChildViewController: sample2ViewController)
+        }
+    }
+    
+    // MARK: - Action
+    
+    @IBAction func tapSegmentedControl(_ sender: UISegmentedControl) {
+        updateView()
+    }
+    
+    // MARK: - Child View Controller Operation Methods
+    
+    private func add(asChildViewController viewController: UIViewController) {
+        // 子ViewControllerを追加
+        addChildViewController(viewController)
+        // Subviewとして子ViewControllerのViewを追加
+        view.addSubview(viewController.view)
+        // 子Viewの設定
+        viewController.view.frame = view.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        // 子View Controllerへの通知
+        viewController.didMove(toParentViewController: self)
+    }
+    
+    private func remove(asChildViewController viewController: UIViewController) {
+        // 子View Controllerへの通知
+        viewController.willMove(toParentViewController: nil)
+        // 子ViewをSuperviewから削除
+        viewController.view.removeFromSuperview()
+        // 子View Controllerへの通知
+        viewController.removeFromParentViewController()
+    }
+    
 }
-
